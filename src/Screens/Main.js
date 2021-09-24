e79b2a8c81467188940c5f684728b2520747140e
Saja Malkawi <sajamalkawi95@gmail.com>
2021-09-25 00:33:31 +0300

    auth + profile + add user to database

--- src/Screens/Main.js
+++ src/Screens/Main.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react'
+import { withAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
 
-export class Home extends Component {
+export class Main extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            user: {}
+        }
+    }
+    componentDidMount = () => {
+        this.props.auth0.getIdTokenClaims().then(tokenResponse => {
+            console.log('hi from did amount')
+            const jwt = tokenResponse.__raw;
+            const config = {
+                headers: {
+                    "Authorization": `Bearer ${jwt}`
+                },
+                method: 'get',
+                baseURL: process.env.REACT_APP_BACKEND_URL,
+                url: '/verify-token'
+            };
+
+            axios(config).then((axiosResponse) => {
+                this.setState({
+                    user: axiosResponse.data
+                })
+            }).catch(error => alert(error))
+
+        }).catch(error => console.log(error));
+    };
+
     render() {
+
         return (
             <div>
                 this is  Main
@@ -10,4 +42,4 @@ export class Home extends Component {
     }
 }
 
-export default Home
+export default withAuth0(Main)
