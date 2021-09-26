e9413401c2e618fcc95cc36c9d8b817e3dbac76a
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-09-26 19:53:38 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,64 +1,53 @@
-import React from 'react';
-import Main from './Screens/Main'
-import Header from './Component/Header'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { withAuth0 } from '@auth0/auth0-react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Footer from './Component/Footer';
-import Login from './Login';
-import Profile from './Screens/Profile';
-import Jewelery from './Screens/Showproduct/Jewelery';
-import WomenClothing from './Screens/Showproduct/WomenClothing';
-import MenClothing from './Screens/Showproduct/MenClothing'
-import Games from './Screens/Games';
-import Electronics from './Screens/Showproduct/Electronics'
-
+import React from "react";
+import Main from "./Screens/Main";
+import Header from "./Component/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { withAuth0 } from "@auth0/auth0-react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./Component/Footer";
+import Login from "./Login";
+import Profile from "./Screens/Profile";
+import Jewelery from "./Screens/Showproduct/Jewelery";
+import WomenClothing from "./Screens/Showproduct/WomenClothing";
+import MenClothing from "./Screens/Showproduct/MenClothing";
+import Games from "./Screens/Games";
+import Electronics from "./Screens/Showproduct/Electronics";
+import "./style.css";
 
 class App extends React.Component {
-
-  render() {
-    return (
-      <>
-        <Router>
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              {
-                this.props.auth0.isAuthenticated ? <Main /> : <Login />
-              }
-
-            </Route>
-            <Route exact path="/profile">
-              <Profile />
-            </Route>
-            <Route exact path="/games">
-              <Games />
-            </Route>
-            <Route exact path="/clothing1">
-              <WomenClothing />
-            </Route>
-            <Route exact path="/jewelery">
-              <Jewelery />
-            </Route>
-            <Route exact path="/electronics">
-              <Electronics />
-            </Route>
-            <Route exact path="/clothing2">
-              <MenClothing />
-            </Route>
-          </Switch>
-          <Footer />
-        </Router>
-      </>
-    )
-  }
+	render() {
+		return (
+			<>
+				<Router>
+					<Header />
+					<Switch>
+						<Route exact path="/">
+							{this.props.auth0.isAuthenticated ? <Main /> : <Login />}
+						</Route>
+						<Route exact path="/profile">
+							<Profile />
+						</Route>
+						<Route exact path="/games">
+							<Games />
+						</Route>
+						<Route exact path="/clothing1">
+							<WomenClothing />
+						</Route>
+						<Route exact path="/jewelery">
+							<Jewelery />
+						</Route>
+						<Route exact path="/electronics">
+							<Electronics />
+						</Route>
+						<Route exact path="/clothing2">
+							<MenClothing />
+						</Route>
+					</Switch>
+					<Footer />
+				</Router>
+			</>
+		);
+	}
 }
 
 export default withAuth0(App);
-
-
-
--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import logo from "../assets/logo_transparent.png"
+import React from "react";
+// import logo from "../assets/logo_transparent.png";
+import { FaFacebookSquare } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
+// import { IconName } from "react-icons/bs";
 
-import { Navbar, NavItem, Image } from 'react-bootstrap';
+// import { Navbar, NavItem, Image } from "react-bootstrap";
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <Navbar collapseOnSelect expand="lg" variant="light">
-                < Navbar.Brand >  Stop and Shop</Navbar.Brand >
-                <Navbar.Brand>
-                    <Image width="50px" height="50px" src={logo} rounded />
-                </Navbar.Brand>
-                <NavItem >contact us  </NavItem>
-            </Navbar >
-        )
-    }
+	render() {
+		return (
+			<div className="footer">
+				<ul>
+					<li>All Right Reversed &copy;2021</li>
+					<li>
+						<FaFacebookSquare className="icon" />
+					</li>
+					<li>
+						<SiGmail className="icon" />
+					</li>
+					<li>
+						<AiFillGithub className="icon" />
+					</li>
+					<li>
+						<AiFillLinkedin className="icon" />
+					</li>
+				</ul>
+			</div>
+		);
+	}
 }
 
 export default Footer;
