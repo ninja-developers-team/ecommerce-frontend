cd1fee64ffa640f5e0ce9a18b1f493a04384be06
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2021-09-27 20:48:57 +0300

    edit

--- src/Screens/Component/ShopingCard.js
+++ src/Screens/Component/ShopingCard.js
@@ -1,28 +1,39 @@
-import React, { Component } from 'react'
-import './card.css'
+import React, { Component } from "react";
+import { AiFillDelete } from "react-icons/ai";
+import "./card.css";
 export class ShopingCard extends Component {
-
-
-    render() {
-
-        return (
-
-            < div class="item" >
-                <div class="buttons">
-                    <span class="delete-btn" onClick={() => { this.props.delFromCard(this.props.item._id) }}></span>
-                </div>
-                <div class="image">
-                    <img src={this.props.item.imagePath} alt="" style={{ height: '70px' }} />
-                </div>
-                <div class="description">
-                    <span>{this.props.item.title}</span>
-                </div>
-                <div class="total-price">{this.props.item.price}</div>
-                <div class="total-price">{this.props.item.quantity}</div>
-            </div>
-
-        )
-    }
+	render() {
+		return (
+			<div class="item">
+				<div class="image">
+					<img
+						src={this.props.item.imagePath}
+						alt=""
+						style={{ height: "70px" }}
+					/>
+				</div>
+				<div class="description">
+					<span>{this.props.item.title}</span>
+				</div>
+				<div class="total-price">{this.props.item.price}</div>
+				<div class="total-price">{this.props.item.quantity}</div>
+				<div class="buttons">
+					<AiFillDelete
+						onClick={() => {
+							this.props.delFromCard(this.props.item._id);
+						}}
+						style={{ cursor: "pointer" }}
+					/>
+					{/* <span
+						class="delete-btn"
+						onClick={() => {
+							this.props.delFromCard(this.props.item._id);
+						}}
+					></span> */}
+				</div>
+			</div>
+		);
+	}
 }
 
-export default ShopingCard
+export default ShopingCard;
